f0d979c6e18a53f9196eb12c055b4fc3744827e7
spacefall <lucarichu@outlook.it>
2025-01-22 00:16:07 +0100

    feat: more accurate bold/italics

--- minmd.js
+++ minmd.js
@@ -9,8 +9,8 @@
 
 //console.log(h("# header\n## header2\n### header3\n#### header4\n##### header5\n###### header6\n####### header7"))
 String.prototype.r=String.prototype.replace
-i = l => l.r(/(\*\*|__)(.*?)\1/g, "<b>$2</b>")				// bold
-    .r(/([*_])(.*?)\1/g, "<i>$1</i>")						// emphasis
+i = l => l.r(/(?<!\\)(\*\*|__)(?=\S)(.*?)(?<=[^\\\s])\1/g, "<b>$2</b>")				// bold
+    .r(/(?<!\\)([*_])(?=\S)(.*?)(?<=[^\\\s])\1/g, "<i>$2</i>") // emphasis
     .r(/!\[(.*?)]\((.*?)\)/g, '<img alt="$1" src="$2"/>')	// image
     .r(/\[(.*?)]\((.*?)\)/g, '<a href="$2">$1</a>')			// link
     .r(/^> (.*)/gm, "<blockquote>$1</blockquote>")			// blockquote
@@ -20,3 +20,4 @@ i = l => l.r(/(\*\*|__)(.*?)\1/g, "<b>$2</b>")				// bold
 	.r(/^(#{1,6}) (.*?)(\n|$)/gm, (_, h, c) => `<h${h.length}>${c}</h${h.length}>`)	// headers
 	.r(/^(.+?)\n=+$/gm, "<h1>$1</h1>")
 	.r(/\n{2,}/g, "<br/>").r(/\s{2,}\n/, "<br/>")			// line breaks
+	.r(/\\([\\*_])/g, "$1")									// unescape
