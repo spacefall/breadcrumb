9b5347c9a602d9abccc34a66baf6ebf6524338f1
spacefall <lucarichu@outlook.it>
2025-01-29 21:21:12 +0100

    feat: reworked rules to better match markdown
    i hate regex

--- minmd.js
+++ minmd.js
@@ -1,23 +1,32 @@
-// h = l =>
-//     l.split`
-// `.map(l => (
-//             v = l.match(/^#{1,6} /)[0].length,
-// 			l[0] == "#" ? `<h${v}>${l.slice(v).trim()}</h${v}>`
-//                 		: l
-//         ))
-//         .join`<br>`
-
-//console.log(h("# header\n## header2\n### header3\n#### header4\n##### header5\n###### header6\n####### header7"))
 String.prototype.r=String.prototype.replace
-i = l => "<p>" + l.r(/(?<!\\)(\*\*|__)(?=\S)(.*?)(?<=[^\\\s])\1/g, "<b>$2</b>")				// bold
-    .r(/(?<!\\)([*_])(?=\S)(.*?)(?<=[^\\\s])\1/g, "<i>$2</i>") // emphasis
-    .r(/!\[(.*?)]\((.*?)\)/g, '<img alt="$1" src="$2"/>')	// image
-    .r(/\[(.*?)]\((.*?)\)/g, '<a href="$2">$1</a>')			// link
-    .r(/^> (.*)/gm, "</p><blockquote><p>$1</p></blockquote><p>")			// blockquote
-    .r(/`(.*)`/g, "<code>$1</code>")						// inline code
-    .r(/^-{3,}$/gm, "<hr/>")								// horizontal rule
-    .r(/^```\w*\n([\s\S]*?)\n```$/gm, "</p><pre><code>$1</code></pre><p>")	// code block
-	.r(/^(#{1,6}) (.*?)(\n|$)/gm, (_, h, c) => `<h${h.length}>${c}</h${h.length}>`)	// headers
-	.r(/^(.+?)\n=+$/gm, "<h1>$1</h1>")
-	.r(/\n{2,}/g, "</p><p>").r(/(\s{2,}\n)|(\\\n)/g, "<br/>")		// line breaks
-	.r(/\\([\\*_])/g, "$1")									// unescape
+i = l => l
+	// tabs to spaces
+	.r(/\t/g, "    ")
+	// hr
+	.r(/^ *[*\-] *[*\-] *[*\-] *$/gm, "<hr/>")
+	// bold
+	.r(/(?<!\\)(\*\*|__)(?=\S)(.*?)(?<=[^\\\s])\1/g, "<b>$2</b>")
+	// italic
+    .r(/(?<!\\)([*_])(?=\S)(.*?)(?<=[^\\\s])\1/g, "<i>$2</i>")
+	// image
+    .r(/!\[(.*?)]\((.*?)\)/g, '<img alt="$1" src="$2"/>')
+	// link
+    .r(/\[(.*?)]\((.*?)\)/g, '<a href="$2">$1</a>')
+	// blockquote
+	.r(/(>+ )(.*)(\n\1.*)*/gm, (m, _1, _2, _3) => `<blockquote>${m.r(/>+ /g, "")}</blockquote>`)
+	// code block
+    .r(/^```\w*\n([\s\S]*?)\n```$/gm, "<pre><code>$1</code></pre>")
+	.r(/( {4})(.*)(\n\1.*)*/gm, (m, _1, _2, _3) => `<pre><code>${m.r(/^ {4}/gm, "")}</code></pre>`)
+	// inline code
+	.r(/`(.*)`/g, "<code>$1</code>")
+	// headers
+	.r(/^(#{1,6}) (.*?)(\n|$)/gm, (_, h, c) => `<h${h.length}>${c}</h${h.length}>`)
+	.r(/^(((.+?)\n)+)=+$/gm, "<h1>$1</h1>")
+	.r(/^(((.+?)\n)+)-+$/gm, "<h2>$1</h2>")
+	// line breaks
+	.r(/( {2,}\n)|(\\\n)|(\n{2,})/g, "<br/>")
+	.r("\n", " ")
+	// unescape
+	.r(/\\([\\*_])/g, "$1")
+	// remove excess spaces
+	.r(/ {2,}/g, " ")
