2188c31aae4c073cf79fb78bb33cdea1fef1762d
spacefall <lucarichu@outlook.it>
2025-01-29 23:04:33 +0100

    feat: added file save

--- main.js
+++ main.js
@@ -10,3 +10,13 @@ s = e => {
     b = setTimeout(() =>  a[+!e].onscroll = () => sync(+!e), 10)
     a[+!e].scrollTop = (a[e].scrollTop/(a[e].scrollHeight-a[e].offsetHeight))*(a[+!e].scrollHeight-a[+!e].offsetHeight)
 }
+
+document.addEventListener("keydown",e=>{
+    if((e.metaKey||e.ctrlKey)&&e.code==="KeyS") {
+        e.preventDefault();m=document.createElement("a");
+        m.href=URL.createObjectURL(new Blob([i.innerText]));
+        m.download="doc.md";
+        m.click();
+        URL.revokeObjectURL(m.href)
+    }
+});
